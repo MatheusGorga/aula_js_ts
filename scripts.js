9f2b05239a9d196905a0cb5901ccfbf5a5ed982d
MatheusGorga <matheusgobar@gmail.com>
2022-09-02 00:11:47 -0300

    arrays

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
-let caneta = {
-  cor: 'azul',
-  tamanho: 18,
-  escrever() {
-    console.log("escrever")
+//https://www.treinaweb.com.br/blog/javascript-metodos-de-arrays-que-voce-precisa-conhecer
+
+let numeros = [1, 2,3,4,5,6,7];
+
+function soma(numeros){
+  let total = 0 
+/*   for (let index = 0; index < numeros.length; index++) {
+    total = total + numeros[index] 
   }
+ */
+  numeros.forEach((n) => {
+      total += n
+  });
+
+  return total
 }
 
-console.log(caneta.cor)
-console.log(caneta.tamanho)
-caneta.escrever()
+console.log(soma(numeros))
