84901394ed67c85d0cbbb0193e97426b28371477
MatheusGorga <matheusgobar@gmail.com>
2022-09-07 22:56:27 -0300

    tratamento de erros

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 //https://www.treinaweb.com.br/blog/javascript-metodos-de-arrays-que-voce-precisa-conhecer
 
-let numeros = [1, 2,3,4,5,6,7];
+let numeros = [1, 2, 3, 4, 5, 6, 7];
 
-function soma(numeros){
-  let total = 0 
-/*   for (let index = 0; index < numeros.length; index++) {
-    total = total + numeros[index] 
-  }
- */
-  numeros.forEach((n) => {
+function soma(numeros) {
+  try {
+    let total = 0
+    numeros.forEach((n) => {
       total += n
-  });
+    });
+    return total
 
-  return total
+  } catch (error) {
+    alert(error)
+  }
+
 }
 
 console.log(soma(numeros))
