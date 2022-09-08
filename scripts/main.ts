a9ac1225e16bbc6a380dd15e651f49f4d7638c6c
MatheusGorga <matheusgobar@gmail.com>
2022-09-08 16:52:12 -0300

    generics

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,13 +1,13 @@
-interface pessoa  {
-  nome: string;
-}
-
-interface pessoa {
-  idade: number;
-}
+var minhaLista: number[] = [1,2,3]
+var minhaListaL: string[] = ['1','2','3']
 
 
-let pessoa : pessoa = {
-  nome: 'asdasd',
-  idade: 10
+function pegaPrimeiro<T>(lista :T[]){
+  return lista[0];
 }
+
+
+
+console.log(pegaPrimeiro<number>(minhaLista))
+console.log(pegaPrimeiro<string>(minhaListaL))
+
